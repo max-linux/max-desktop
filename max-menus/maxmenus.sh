773f9bb999c8891fc6fdb77c2259e175a926ac5f
madrid-linux <max@educa.madrid.org>
2008-01-02 19:52:25 +0000

    mario.izquierdo max-menus (3.1.20080102.max2)   * Add maxmenus script (copy menus in users home)   * Remove not needed code in debian/[postinst|prerm|postrm]   * Add Math menu in .config/menus/applications.menu   * Remove not needed launchers in usr/share/max/applications
    git-svn-id: http://max.educa.madrid.org:8000/svn/trunk@52 424cea3e-0240-0410-9575-8bd47b58cd33

---
+++ max-menus/maxmenus.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+VERSION=__VERSION__
+
+if [ $(id -u) != 0 ]; then
+  echo "You aren't root user. Access denied."
+  exit 1
+fi
+
+decho() {
+  echo "maxmenus:: $@" >&2
+}
+
+usage() {
+  echo "$0 usage:"
+  echo "           $0 --update (update all home menus)"
+  echo "           $0 --purge (try to remove all home menus)"
+}
+
+case $1 in
+   --update)
+     ACTION=update
+     ;;
+   --purge)
+     ACTION=purge
+     ;;
+   *)
+     usage
+     exit 1
+     ;;
+esac
+
+USER_MENUS=".config/menus"
+USER_LOCAL=".local/share/desktop-directories"
+SKEL_MENUS="/etc/skel/.config/menus"
+SKEL_LOCAL="/etc/skel/.local/share/desktop-directories"
+
+for home in /home/*; do
+
+    user=$(basename $home)
+
+    # look for lock file
+    if [ -e $home/.maxmenus.lock ]; then
+      decho "No updating menus, found $home/.maxmenus.lock"
+      continue
+    fi
+
+    mkdir -p $home/$USER_MENUS
+    mkdir -p $home/$USER_LOCAL
+
+    for file in $SKEL_MENUS/*; do
+      [ "$ACTION" = "update" ] && cp $file $home/$USER_MENUS/
+      [ "$ACTION" = "purge" ] &&  rm -f $home/$USER_MENUS/$(basename $file)
+    done
+
+    for file in $SKEL_LOCAL/*; do
+      [ "$ACTION" = "update" ] && cp $file $home/$USER_LOCAL/
+      [ "$ACTION" = "purge" ] &&  rm -f $home/$USER_LOCAL/$(basename $file)
+    done
+
+    # check for permissions
+    chown $user $home/$USER_MENUS/* 2>/dev/null
+    chown $user $home/$USER_LOCAL/* 2>/dev/null
+
+done # end of for $home
