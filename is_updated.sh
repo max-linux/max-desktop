2a76bae99d3524b99c489cd76d3634f09fd0b24f
madrid-linux <max@educa.madrid.org>
2008-04-27 08:58:21 +0000

    mario.izquierdo   * Add is_updated.sh to get not updated packages
    git-svn-id: http://max.educa.madrid.org:8000/svn/trunk@244 424cea3e-0240-0410-9575-8bd47b58cd33

---
+++ is_updated.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+CHANGELOGS=$(find ./ -maxdepth 3 -mindepth 3 -name "changelog")
+
+for change in $CHANGELOGS; do
+  source=$(dpkg-parsechangelog -l$change | awk '/^Source/ {print $2}')
+  version=$(dpkg-parsechangelog -l$change | awk '/^Version/ {print $2}')
+  letter=$(echo $source| cut -c1)
+  pkgdir="../../../max40/pool/main/$letter/$source/"
+  #echo "source=$source version=$version"
+
+  if [ -d $pkgdir ]; then
+    dscpkg=$(find $pkgdir -name "$source*dsc")
+    dscversion=$(basename $dscpkg | awk -F "_" '{print $2}' | sed 's/.dsc//g')
+    if [ "$version" != "$dscversion" ]; then
+      echo " ** WARNING ** obsolete package in mirror $source $version old=$dscversion"
+    fi
+  else
+    echo "**WARNING $source is not in mirror"
+  fi
+done
