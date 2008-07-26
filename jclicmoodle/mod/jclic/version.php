a6b9ed783f3e4f3e530bc6dc52117caff0c0a69a
madrid-linux <max@educa.madrid.org>
2008-07-26 14:23:36 +0000

    mario.izquierdo   * import jclicmodule into repos
    git-svn-id: http://max.educa.madrid.org:8000/svn/trunk@335 424cea3e-0240-0410-9575-8bd47b58cd33

---
+++ jclicmoodle/mod/jclic/version.php
@@ -0,0 +1,10 @@
+<?PHP  // $Id: version.php,v 1.6 2008/01/24 15:31:11 sarjona Exp $
+
+/////////////////////////////////////////////////////////////////////////////////
+///  Code fragment to define the version of jclic
+///  This fragment is called by moodle_needs_upgrading() and /admin/index.php
+/////////////////////////////////////////////////////////////////////////////////
+
+$module->version  = 2008012300;  // The current module version (Date: YYYYMMDDXX)
+$module->cron     = 0;           // Period for cron to check this module (secs)
+?>
