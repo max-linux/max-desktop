3b016ebdadf3843be50de3c8c893ea49a2e7a61c
Mario Izquierdo (mariodebian) <mariodebian@gmail.com>
2014-05-09 21:13:08 +0200

    ubiquity-frontend-max: (7.0.max42)
      * Disable repos during install, only use DVD

--- ubiquity-frontend-max/usr/share/ubiquity/plugininstall.py
+++ ubiquity-frontend-max/usr/share/ubiquity/plugininstall.py
@@ -452,7 +452,11 @@ class Install(install_misc.InstallBase):
             fe00::0 ip6-localnet
             ff00::0 ip6-mcastprefix
             ff02::1 ip6-allnodes
-            ff02::2 ip6-allrouters""")
+            ff02::2 ip6-allrouters
+
+            127.0.0.1 max.educa.madrid.org
+            127.0.0.1 archive.ubuntu.com
+            127.0.0.1 security.ubuntu.com""")
         hosts.close()
 
         # Network Manager's ifupdown plugin has an inotify watch on
