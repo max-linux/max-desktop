39027de7ba738cc103aa00ad802f7cc10e8c51a5
Mario Izquierdo (mariodebian) <mariodebian@gmail.com>
2018-05-12 11:18:58 +0200

    max-ubufox: (3.2.3max4)
      * Fix homepage in max-firefox-conf => distribution.ini
      * Remove extension, firefox > 57 incompatible, add pref to set
        homepage
      * Update version 9.0 => 9.5

---
+++ max-ubufox/debian/max-ubufox-pref.js
@@ -0,0 +1,14 @@
+// Homepage
+pref("browser.startup.homepage", "file:///usr/share/doc/max-ubufox/startpage.html");
+
+// supress override
+pref("browser.startup.homepage_override.mstone", "ignore");
+pref("startup.homepage_override_url", "file:///usr/share/doc/max-ubufox/startpage.html");
+
+
+// disable welcome
+pref("startup.homepage_welcome_url", "file:///usr/share/doc/max-ubufox/startpage.html");
+
+
+// new tab sites
+pref("browser.newtabpage.activity-stream.default.sites", "https://www.educa2.madrid.org,http://external.educa2.madrid.org/web/max/inicio,http://herramientas.educa.madrid.org/foros/viewforum.php?f=20");
