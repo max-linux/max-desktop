2be6966b0aa31e2c855e487920a38363c6892c11
CAU MAX <cau_max@educa.madrid.org>
2023-04-10 12:04:20 +0200

    max-ubufox: (3.4.0max16)
      * Build in MAX 12.0
      * Change MAX 11.5.1 => MAX 12.0
      * new version updated
      * changelog version fixed
      * Build in MAX 12.0
      * Changelog version fixed
      * Start page version fixed

--- max-ubufox/compare.sh
+++ max-ubufox/compare.sh
@@ -5,6 +5,7 @@ ORIG_PATH=/home/madrid/MaX/build/firefox4/ubufox-0.9/
 ORIG_PATH=/home/madrid/MaX/build/ubufox/ubufox-1.5/
 ORIG_PATH=/data/max/build/desktop/ubufox/ubufox-3.0/
 ORIG_PATH=/data/max/build/desktop/ubufox/ubufox-3.2/
+ORIG_PATH=/data/max/build/desktop/ubufox/ubufox-3.4/
 
 #for f in $(find usr/ -type f -name "*py"); do
 for e in $(find $ORIG_PATH -type f | grep -v -e ".svn" -e ".pc"); do
